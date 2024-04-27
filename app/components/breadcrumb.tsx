0b423232443fc1afac245689e15d40ed80d7f627
matcher9131 <matcher9131cd@gmail.com>
2024-04-27 11:51:50 +0900

    Implement page: Home #12

--- app/components/breadcrumb.tsx
+++ app/components/breadcrumb.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-// import Link from "next/link";
+import { Cormorant_Garamond } from "next/font/google";
 import { usePathname } from "next/navigation";
 import { getPageName } from "../_shared/pageProperties";
+import { classes } from "../util/classes";
 import { TransitionLink } from "./pageTransition/transitionLink";
+
+const contentFont = Cormorant_Garamond({
+    weight: "500",
+    subsets: ["latin"],
+});
 
 const Breadcrumb = (): JSX.Element | null => {
     const path = usePathname();
@@ -27,9 +33,13 @@ const Breadcrumb = (): JSX.Element | null => {
                 {paths.map((element) => (
                     <li key={element}>
                         {element !== path ? (
-                            <TransitionLink href={element}>{getPageName(element) ?? element}</TransitionLink>
+                            <TransitionLink href={element} className={classes(contentFont.className, "text-xl")}>
+                                {getPageName(element) ?? element}
+                            </TransitionLink>
                         ) : (
-                            <span>{getPageName(element) ?? element}</span>
+                            <span className={classes(contentFont.className, "text-xl")}>
+                                {getPageName(element) ?? element}
+                            </span>
                         )}
                     </li>
                 ))}
