01562ab8dae510ccc93625a1d9dfd83d215ce045
matcher9131 <matcher9131cd@gmail.com>
2025-09-26 12:08:04 +0900

    Fix layouts of works/page (Apply responsive design to number of columns of Lineup table)

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -20,12 +20,12 @@ const Works = (): JSX.Element => {
             ></iframe>
 
             <h2>Lineup</h2>
-            <section className="flex gap-x-4">
+            <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
                 {getWorksPages().map((page) => (
                     <TransitionLink
                         key={page.path}
                         href={page.path}
-                        className="card card-compact w-72 transition-all hover:-translate-y-1 hover:bg-base-200"
+                        className="card card-compact w-full min-w-72 transition-all hover:-translate-y-1 hover:bg-base-200"
                     >
                         <figure>ここに画像</figure>
                         <div className="card-body">
