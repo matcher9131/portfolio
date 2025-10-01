6cfffadc8e603e6e628ef81b083c8d2e1ed9a414
matcher9131 <matcher9131cd@gmail.com>
2025-10-01 11:24:49 +0900

    Fix next.config.mjs (use basePath)

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 import withExportImages from "next-export-optimize-images";
 
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = withExportImages({
     output: "export",
+    basePath: isProd ? "/portfolio" : "",
 });
 
 export default nextConfig;
